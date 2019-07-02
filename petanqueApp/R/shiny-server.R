22fdb6d324b9f9547b285c38867a6c7c607c48bf
Maxim Nazarov <maxim.nazarov@openanalytics.eu>
2019-07-02 16:20:23 +0200

    added player names entry

--- petanqueApp/R/shiny-server.R
+++ petanqueApp/R/shiny-server.R
@@ -27,8 +27,9 @@ petanqueServer <- function(input, output, session) {
         h2(
             span(if (gameEnded()) "Game finished!"),
             span(if (!gameActive()) "Press \"Start\" to start the game!" else "Game is in progress!"),
+            span(if (!is.null(players())) "Playing:", paste(players(), collapse = " and ")),
             span(if (!is.null(turnNumber())) paste("Turn Number:", turnNumber())),
-            span(if (!is.null(activePlayer())) paste("Current player:", activePlayer()))
+            span(if (!is.null(activePlayer())) paste("Current player:", players()[[activePlayer()]]))
         )
       })
   
@@ -50,8 +51,9 @@ petanqueServer <- function(input, output, session) {
         
         tagList(
             if (gameEnded()) {
-              span(paste0("Game finished with Player ", winner(), " winning with ", score(), " points."),
-                  actionLink("gotoRankings", "See Rankings."), 
+              span(
+                  paste0("Game finished with Player ", winner(), "(", players()[[winner()]], ") winning with ", score(), " points."),
+                  actionLink("gotoRankings", "See Rankings.") 
               )
             } else {
               paste("Player", 3-activePlayer(), "chose", chosenDistr())
@@ -78,6 +80,7 @@ petanqueServer <- function(input, output, session) {
   
   gameActive <- reactiveVal(FALSE)
   gameEnded <- reactiveVal(FALSE)
+  players <- reactiveVal(NULL)
   activePlayer <- reactiveVal(NULL)
   turnNumber <- reactiveVal(NULL)
   MAX_TURNS <- 6
@@ -98,11 +101,14 @@ petanqueServer <- function(input, output, session) {
   # inputs
   observeEvent(input$enter, {
         if (!gameActive()) {
-          # just started the game
+          ## just started the game
+          players(NULL)
           gameEnded(FALSE)  
           gameActive(TRUE)
+          # TODO: ask for player names
+          showModal(playerInfoModal())
           # sample the target
-          targetLoc(runif(1, 3, 9))
+          targetLoc(sampleTarget())
           # Set active player
           activePlayer(1)
           # Set turn
@@ -110,9 +116,10 @@ petanqueServer <- function(input, output, session) {
           # Sample distribution
           distrChoices(sampleDistribution())
         } else {
-          # have chosen a distribution   
-          # TODO: make a throw
+          ## have chosen a distribution   
+          # TODO: show a throw
           chosenDistr(distrChoices()[[activeDistr()]])
+          
           ## Switch player:
           # change the player
           curPlayer <- activePlayer()
@@ -157,9 +164,83 @@ petanqueServer <- function(input, output, session) {
         }
       })
   
+  observeEvent(input$confirmPlayers, {
+        if (nchar(input$player1) == 0) {
+          showModal(playerInfoModal(failed1 = TRUE))
+        } else if (nchar(input$player2) == 0) {
+          showModal(playerInfoModal(failed2 = TRUE))
+        } else if (input$player1 == input$player2) {
+          showModal(playerInfoModal(failedBoth = TRUE))
+        } else {
+          removeModal()
+          
+          playerNames <- c(input$player1, input$player2)
+          
+          # save names in the DB
+          oldPlayers <- getPlayers()
+          if (!all(playerNames %in% oldPlayers))
+            addPlayers(playerNames)
+          # use names for the game
+          players(playerNames)
+        }
+      })
+  
+}
+
+playerInfoModal <- function(failed1 = FALSE, failed2 = FALSE, failedBoth = FALSE) {
+  allPlayers <- getPlayers()
+  
+  selectizeOpts <- list(placeholder = "please choose or enter new", create = TRUE,
+      onInitialize = I('function() { this.setValue(""); }'))
+  
+  modalDialog(title = "Players", 
+
+      if (failedBoth)
+        span("Please choose two different names", class = "warn"),
+      
+      if (failed1)
+        span("Invalid player 1 name", class = "warn"),
+      
+      selectizeInput("player1", "Player 1:", choices = allPlayers, 
+          multiple = FALSE, options = selectizeOpts),
+
+      if (failed2)
+        span("Invalid player 2 name", class = "warn"),
+      
+      selectizeInput("player2", "Player 2:", choices = allPlayers, 
+          multiple = FALSE, options = selectizeOpts),
+
+      footer = actionButton("confirmPlayers", "Start!"), 
+      size = "m", 
+      easyClose = FALSE)
+}
+
+PLAYER_FILE <- "/tmp/petanque-players.rds"
+
+getPlayers <- function(file = PLAYER_FILE) {
+  if (file.exists(file))
+    out <- readRDS(file)
+  else 
+    out <- c()
+  out
+}
+
+addPlayers <- function(players, file = PLAYER_FILE) {
+  if (file.exists(file))
+    out <- readRDS(file)
+  else 
+    out <- c()
+  
+  out <- unique(c(out, players))
+  saveRDS(out, file)  
 }
 
 sampleDistribution <- function() {
-  # TODO: distr selection
+  # TODO: distribution selection
   sample(c("normal(4, 3)", "normal(5, 2)", "poisson(6)", "poisson(8)", "gamma(6, 2)", "gamma(5, 1)"), size = 3, replace = FALSE)
+}
+
+sampleTarget <- function() {
+  # TODO
+  runif(1, 3, 9)
 }
