c1aeb2c708bc121d26d13f5fe021f1f2c9d42251
Maxim Nazarov <maxim.nazarov@openanalytics.eu>
2019-07-04 15:02:04 +0200

    do not error if no sound

--- petanqueApp/R/sounds.R
+++ petanqueApp/R/sounds.R
@@ -1,15 +1,15 @@
 ## sound effects
 #' @importFrom beepr beep
 playSound <- function(which = c("ball", "win", "hit")) {
-  which <- match.arg(which)
-  
   file <- switch(which,
       "ball" = "button-pressed.ogg",
       "win"  = "fanfare.wav",
-      "hit"  = "hit.m4a")
+      "hit"  = "hit.m4a",
+      NULL)
   
-  path <- system.file("sounds", file, package = "petanqueApp")
-  
-  beep(path)
+  if (!is.null(file)) {
+    path <- system.file("sounds", file, package = "petanqueApp")
+    try(beep(path), silent = TRUE)
+  }
   
 }
